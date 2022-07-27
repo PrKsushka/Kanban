0a8bc380cbdf9bf475f24a428010dd5f1f84b607
PrKsushka <ksushkaprot@gmail.com>
2022-07-27 13:06:48 +0300

    add queries for status

--- server/db/controllers/note/getDataAboutNotes.ts
+++ server/db/controllers/note/getDataAboutNotes.ts
@@ -3,7 +3,16 @@ import Notes from "../../models/note";
 
 const getDataAboutNotes = async (req: Request, res: Response) => {
   try {
-    const notes = await Notes.find({});
+    let notes;
+    if (req.query.status === "do") {
+      notes = await Notes.find({ status: "do" });
+    } else if (req.query.status === "process") {
+      notes = await Notes.find({ status: "process" });
+    } else if (req.query.status === "done") {
+      notes = await Notes.find({ status: "done" });
+    } else {
+      notes = await Notes.find({});
+    }
     res.status(200).json(notes);
   } catch (e: any) {
     res.status(e.statusCode).json(e.message);
